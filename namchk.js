e408802b743ee1be2d623e4a6675b08ebd56fe9e
Youssef Awad <shadowblade7536@gmail.com>
2017-06-16 16:54:52 +0400

    I have no clue anymore

--- namchk.js
+++ namchk.js
@@ -14,80 +14,80 @@ var info = chalk.bold.blue
 
 //parser
 const parser = new ArgumentParser({
-  version: '0.1',
-  addHelp: true,
-  epilog: "Use wisely."
+	version: '0.1',
+	addHelp: true,
+	epilog: "Use wisely."
 });
 
 parser.addArgument(
-  ['-u', '--username'], {
-    help: 'The username to search on namechk.',
-    nargs: 1,
-    metavar: "username"
-  }
+	['-u', '--username'], {
+		help: 'The username to search on namechk.',
+		nargs: 1,
+		metavar: "username"
+	}
 );
 
 var args = parser.parseArgs();
 
 if (args.username !== null) {
-  if (args.username[0].length > 0) {
-    target = args.username[0]
-    runSearch(args.username[0])
-  } else {
-    console.log("Please specify a username!")
-    process.exit()
-  }
+	if (args.username[0].length > 0) {
+		target = args.username[0]
+		runSearch(args.username[0])
+	} else {
+		console.log("Please specify a username!")
+		process.exit()
+	}
 } else {
-  console.log("Please specify a username!")
-  process.exit()
+	console.log("Please specify a username!")
+	process.exit()
 }
 
 function runSearch(username) {
-  //Init spinner
-  //const spinner = ora("Initializing...").start();
+	//Init spinner
+	//var spinner = ora("Initializing...").start();
 
-  //init horseman
-  var horseman = new Horseman({
-    loadImages: false,
-    diskCache: true,
-    timeout: 30000,
-    injectJquery: false
-  })
+	//init horseman
+	var horseman = new Horseman({
+		loadImages: false,
+		diskCache: true,
+		timeout: 30000,
+		injectJquery: false
+	})
 
-  horseman
-    .open('https://www.namechk.com')
-    .waitForSelector('.search-field')
-    .click('.search-field')
-    .type('input[name=q]', username)
-    .click('.search-btn')
-    .waitForSelector('.unavailable')
-    .wait(40000)
-    .evaluate(function() {
-      var services = $('.service')
-      var results = []
-      for (var i = 0; i < services.length; i++) {
-        var object = {
-          html: services[i].outerHTML,
-          name: services[i].innerText,
-          innerhtml: services[i].innerHTML
-        }
-        results.push(object);
-      }
-      return results
-    })
-    .then(function(data) {
-      //spinner.text = 'Parsing information...'
-      for (var i = 0; i < data.length; i++) {
+	horseman
+		.open('https://www.namechk.com')
+		.waitForSelector('.search-field')
+		.click('.search-field')
+		.type('input[name=q]', username)
+		.click('.search-btn')
+		.waitForSelector('.unavailable')
+		.wait(40000)
+		.evaluate(function () {
+			var services = $('.service')
+			var results = []
+			for (var i = 0; i < services.length; i++) {
+				var object = {
+					html: services[i].outerHTML,
+					name: services[i].innerText,
+					innerhtml: services[i].innerHTML
+				}
+				results.push(object);
+			}
+			return results
+		})
+		.then(function (data) {
+			//spinner.text = 'Parsing information...'
+			for (var i = 0; i < data.length; i++) {
 
-        var service = data[i].html.match('unavailable')
-        if (service !== null) {
-          console.log(data[i].name)
-        }
+				var service = data[i].html.match('unavailable')
+				if (service !== null) {
+					console.log(data[i].name)
+				}
 
-        var failed = data[i].html.match('failed')
-        if (failed !== null) {
-          console.log("FAIL! " + data[i].name)
-        }
-      }
-    })
+				var failed = data[i].html.match('failed')
+				if (failed !== null) {
+					console.log("FAIL! " + data[i].name)
+				}
+			}
+		})
 }
